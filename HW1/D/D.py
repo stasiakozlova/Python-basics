7526b2db3b79eed0dd1c329edd951df1e7cbc6a8
Stasia <stasia.kozlova@gmail.com>
2022-02-19 20:37:13 +0300

    homework1

--- HW1/D/D.py
+++ HW1/D/D.py
@@ -1,3 +1,8 @@
 def solution(total):
-    ### YOUR CODE HERE ###
+    total = total % 3600
+    hours = total // 60
+    minutes = total % 60
+    if minutes < 10:
+        minutes = '0' + str(minutes)
+    print(f'{hours} {minutes}')
     return
