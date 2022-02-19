67c27596bbdf45d2c8a21f01e8ba54b046147435
Stasia <stasia.kozlova@gmail.com>
2022-02-19 20:30:36 +0300

    homework1

--- HW1/B/B.py
+++ HW1/B/B.py
@@ -1,3 +1,12 @@
 def solution(n):
-    ### YOUR SOLUTION ###
+    x1 = '   _~_   '
+    x2 = '  (o o)  '
+    x3 = ' /  V  \ '
+    x4 = '/(  _  )\\'
+    x5 = '  ^^ ^^  '
+    print(x1 * n)
+    print(x2 * n)
+    print(x3 * n)
+    print(x4 * n)
+    print(x5 * n)
     return
