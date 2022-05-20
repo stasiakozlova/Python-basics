e35c812a42fd5feb90d6cd424d143995535c97fc
Stasia <stasia.kozlova@gmail.com>
2022-05-20 14:03:23 +0300

    Коммит

--- HW3/1.py
+++ HW3/1.py
@@ -4,8 +4,13 @@ class OneIndexedList:
     def __init__(self, items=None):
         self.items = items or []
 
-    def __setitem__(self, element):
-        self.items.append(element)
-        
+    def __setitem__(self, index, element):
+        self.items[index-1] = element
+
     def __getitem__(self, index):
         return self.items[index-1]
+
+
+array = OneIndexedList([56, 76, 74])
+array[1] = 3
+print(array[1])
